75f11cd74a7b3e25c346d2e6c90c33c136ba4023
shay markovich <shaymark@gmail.com>
2021-04-22 09:07:54 +0300

    Initial Commit

---
+++ Shared/gk8App.swift
@@ -0,0 +1,17 @@
+//
+//  gk8App.swift
+//  Shared
+//
+//  Created by Shay markovich on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct gk8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
